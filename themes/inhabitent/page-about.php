bfd4544d14d82b6f0c1a06ad81a97edd91112dfc
Anna Kviese <anna.kviese@gmail.com>
2016-11-08 09:07:04 -0800

    added about page

---
+++ themes/inhabitent/page-about.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+			 <section class = "about-image">
+            </section>
+		
+		
+
+<?php get_footer(); ?>
