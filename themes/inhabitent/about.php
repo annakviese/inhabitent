b7be20ff22d5574595d78227c10e86cbecc8a1f2
Anna Kviese <anna.kviese@gmail.com>
2016-11-08 20:09:39 -0800

    added style to about image

--- themes/inhabitent/about.php
+++ themes/inhabitent/about.php
@@ -5,12 +5,12 @@
  */
 
 get_header(); ?>
-        <div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+        
 
 			 <section class = "about-hero">
             </section>
-
+        <div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
 
             <div = "about-text">
             <?php
