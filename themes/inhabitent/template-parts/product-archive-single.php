87c6f8f7b7079c3e5f8f8f62c070b53578659ac5
Anna Kviese <anna.kviese@gmail.com>
2016-11-09 21:40:47 -0800

    started styling individual product page

--- themes/inhabitent/template-parts/product-archive-single.php
+++ themes/inhabitent/template-parts/product-archive-single.php
@@ -8,29 +8,32 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="archive-header">
+		
+		<header class="entry-header">
 		<?php if ( has_post_thumbnail() ) : ?>
 			<?php the_post_thumbnail( 'large' ); ?>
 		<?php endif; ?>
+		</header><!-- .entry-header -->
 
-		<?php the_title( '<h1 class="archive-title">', '</h1>' ); ?>
+		<div class = "content-wrapper">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
-		<div class="archive-meta">
-			<?php red_starter_posted_on(); ?> / <?php red_starter_comment_count(); ?> / <?php red_starter_posted_by(); ?>
-		</div><!-- .entry-meta -->
-	</header><!-- .entry-header -->
+			<div class="entry-meta">
+				<?php red_starter_posted_on(); ?> / <?php red_starter_comment_count(); ?> / <?php red_starter_posted_by(); ?>
+			</div><!-- entry-meta -->
 
-	<div class="archive-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="archive-links">' . esc_html( 'Pages:' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+			<div class="entry-content">
+				<?php the_content(); ?>
+			<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html( 'Pages:' ),
+					'after'  => '</div>',
+				) );
+			?>
+			</div><!-- .entry-content -->
+		</div> <!-- content-wrapper -->
 
-	<footer class="archive-footer">
+	<footer class="entry-footer">
 		<?php red_starter_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
