b5a5d3a2aaeb5dba1c69c0bbb4fb2d8fa6ea80b6
Anna Kviese <anna.kviese@gmail.com>
2016-11-13 20:45:37 -0800

    styled product taxonomy page

--- themes/inhabitent/taxonomy-product_type.php
+++ themes/inhabitent/taxonomy-product_type.php
@@ -8,8 +8,7 @@
 get_header(); ?>
     <p> taxonomy-product</p>
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-		<p> archive.php</p> 
+		
 		<?php if ( have_posts() ) : ?>
 
 			<header class="page-header">
@@ -19,24 +18,34 @@ get_header(); ?>
 				?>
 			</header><!-- .page-header -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+			
+				<article class= id="post-<?php the_ID(); ?>"<?php post_class(); ?>>
 
-				<?php
-					get_template_part( 'template-parts/content' );
-				?>
-			<?php endwhile; ?>
+					<div class = "archive-area">
+        			<?php while ( have_posts() ) : the_post(); ?>
+					
+					<div class = "product-post">
+            			<div class = "product-thumbnail">
+							<a href="<?php the_permalink() ; ?> "><?php the_post_thumbnail( 'large' ); ?></a>
+						</div>
+         				
+						 <div class = "product-info">  
+         					<p class = "archive-title"> 
+								<?php the_title('<p class="archive-title">'); ?> 
+              					<?php echo CFS()->get( 'product_price' ); ?> 
+							</p>
+        				</div>
+					</div> <!-- product-post -->
+					<?php endwhile ?>
+				</div>
+				</article><!-- #post-## -->
 
-			<?php the_posts_navigation(); ?>
+					<?php else : ?>
+						<?php get_template_part( 'template-parts/content', 'none' ); ?>
+					<?php endif; ?>
+			
 
-		<?php else : ?>
-
-			<?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+
 <?php get_footer(); ?>
