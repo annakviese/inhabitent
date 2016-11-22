d2902858796cd85b1ff04802f5abea60e77b136f
Anna Kviese <anna.kviese@gmail.com>
2016-11-21 16:45:40 -0800

    fixed the header navigation

--- themes/inhabitent/header.php
+++ themes/inhabitent/header.php
@@ -21,22 +21,23 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding">
+				<div class="header-nav">
+					<div class="site-branding">
+						
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+		<!--                   <img src="<?php echo get_template_directory_uri() //. '/images/logos/inhabitent-logo-tent.svg'; ?>" class="logo" alt="Inhabitent company logo" /> -->
+					</a>
 					
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-    <!--                   <img src="<?php echo get_template_directory_uri() //. '/images/logos/inhabitent-logo-tent.svg'; ?>" class="logo" alt="Inhabitent company logo" /> -->
-                   </a>
-				   
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
-				</div><!-- .site-branding -->
+						<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+					</div><!-- .site-branding -->
 
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-					<div class="header-search"><?php get_search_form(); ?></div>
-				</nav><!-- #site-navigation -->
-
-			</header><!-- #masthead -->
+					<nav id="site-navigation" class="main-navigation" role="navigation">
+						<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
+						<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
+						<div class="header-search"><?php get_search_form(); ?></div>
+					</nav><!-- #site-navigation -->
+				</div>
+			</header><!-- site-header -->
 
 			<div id="content" class="site-content">
